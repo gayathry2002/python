adc845c69c3b261f2b5a8a66a40367edc1aac9a7
gayathry <gayathripalliyath@gmail.com>
2023-08-03 17:43:10 +0530

    message

---
+++ sample.py
@@ -0,0 +1,43 @@
+# a=input("enter your named:")
+# print("your name is:",a)
+
+# c = int(input("enter a no."))
+# d=int(input("enter a no."))
+# print(type(c))
+# sum = c+d
+# print(sum)
+
+# a=int(input("enter a no:"))
+# b=int(input("enter a no:"))
+# mul=a*b
+# print(mul)
+
+
+
+
+# a=50
+# b=20
+# # a+=b
+# print(a)
+# print(not(b==20))
+# print(10<a and a==50)
+# print(70<a or a==50)
+
+# a="drinking"
+# b="drinKing"
+# print(a is b)
+# print("K" in b)
+# print(a is not b)
+# a=4 
+# b=7
+# print(a & b)
+# print(bin(b)) 
+
+a="computer"
+print(a[0])
+print(a[0:4])
+print(a[-1])
+print(a[-2])
+print(a[::-1])
+print(a[ :7])
+print(a[2: ])
