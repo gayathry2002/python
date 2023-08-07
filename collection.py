466ea6b35548e6d09be038d2bd87e138033aefb2
gayathry <gayathripalliyath@gmail.com>
2023-08-07 11:32:59 +0530

    message

---
+++ collection.py
@@ -0,0 +1,9 @@
+a=[10,20,30,3.5,"abc"]
+a.append(100)
+print(a)
+a.insert(2,"python")
+print(a)
+a[4]=300
+print(a)
+print(a[5])
+print(a[-1])
---
+++ list.py
@@ -0,0 +1,17 @@
+# a=[10,20,30,40,50,60]
+# b=20
+# c=40
+# a[3]=b
+# a[1]=c
+# print(a)
+# sum=a[1]+a[5]
+# print(sum)
+
+
+
+a=[10,20,10,10,30,50,78]
+n=len(a)
+print("length of above list is:", n)
+
+average=sum(a)/len(a)
+print("average of list: ",average)
