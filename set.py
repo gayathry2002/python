90d0d1ba964acf373cff03abac8b2a0635f3ee62
gayathry <gayathripalliyath@gmail.com>
2023-08-08 17:53:11 +0530

    message

---
+++ set.py
@@ -0,0 +1,34 @@
+# s={1,2,3,"abc",5.5}
+# print(s)
+# s.add(8)
+# print(s)
+# n={20,30,"xyz"}
+# s.update(n)
+# print(s)
+
+# s={10,30,"apple","orange",55}
+# s.remove('orange')
+# print(s)
+
+# s={10,30,"apple","orange",55}
+# n={100,200,300}
+# s.update(n)
+# print(s)
+
+
+# set1={"python","data","keywords"}
+# set2={"abc","java"}
+# set3=set1.union(set2)
+# print(set3)
+
+# s={10,30,"apple","orange",55}
+# len=len(s)
+# print("length of set is:",len)
+
+# s={10,30,"apple","orange",55}
+# s.clear()
+# print(s)
+
+s={10,30,"apple","orange",55}
+del s
+print(s)
---
+++ tuple.py
@@ -0,0 +1,35 @@
+# t1=(1,2,3,4)
+# t2=(5,6,7)
+# # t3=t1+t2
+# list(t1)
+# l=list(t1)
+# l.append(5)
+# l.append(6)
+# l.append(7)
+# t3=tuple(l)
+# print(t3)
+
+# t=(20,30,45,8)
+# sum=sum(t)
+# print(sum)
+
+# t=(20,30,45,8)
+# list(t)
+# l=list(t)
+# l[2]=100
+# t=tuple(l)
+# print(t)
+
+# t=(20,30,45,8)
+# list(t)
+# l=list(t)
+# l.pop(3)
+# t=tuple(l)
+# print(t)
+
+# t=(20,30,45,8)
+# print(30 in t)
+
+t=(20,30,45,8)
+a=len(t)
+print("length of tuple:",a)
