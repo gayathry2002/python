3dafdb6c9ede353b975c4d6ef7527c6c4d6d0755
gayathry <gayathripalliyath@gmail.com>
2023-08-21 18:01:52 +0530

    message

--- pattern/pattern.py
+++ pattern/pattern.py
@@ -3,10 +3,41 @@
 # print('*','*','*','*')
 # print('*','*','*','*')
 
-# for i in range(1,6):
+# for i in range(1,5):
 #     print('* '*4)
 
 
-for i in range(1,5):
-    print('* '*i)
+# for i in range(1,5):
+#     print('* '*i)
+
+
+# n=4
+# for i in range(1,5):
+#    n-=1
+#    print('  ' * n + ' *'*i)      
+
+
+# n=4
+# for i in range(1,5):
+#     n-=1
+#     print(' ' * n + ' *'*i)
     
+
+# n=5
+# for i in range(1,5):
+#     n-=1
+#     print('*  '*n)
+    
+# for i in range(5,0,-1):
+#     print('* '*i)
+
+# for i in range(1,10,2):
+#     print(i)
+
+
+
+
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(j,end='')
+    print()    
