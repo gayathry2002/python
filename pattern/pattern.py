b6afd0381e2d833d000cba00be37002e4db41e26
gayathry <gayathripalliyath@gmail.com>
2023-08-23 17:57:46 +0530

    message

--- pattern/pattern.py
+++ pattern/pattern.py
@@ -37,7 +37,22 @@
 
 
 
-for i in range(1,5):
-    for j in range(1,i+1):
-        print(j,end='')
-    print()    
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(j,end='')
+#     print() 
+
+
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#             print(i,end=' ')
+#     print()    
+
+
+for i in range(1,6):
+    for j in range(1,2*i):
+         if j % 2 != 0:
+             print(j,end=' ')
+    print() 
+
