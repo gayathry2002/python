a4d53fde851183180fc3a7da5c27ce00664142b8
gayathry <gayathripalliyath@gmail.com>
2023-09-11 05:12:41 -0700

    message

---
+++ function/lambda.py
@@ -0,0 +1,13 @@
+# w=lambda a, b:a+b
+# x=lambda a, b:a-b
+# y=lambda a, b:a*b
+# z=lambda a, b:a/b
+# a=int(input("enter a no:"))
+# b=int(input("enter a no:"))
+# print("sum:",w(a,b))
+# print("sub:",x(a,b))
+# print("mul:",y(a,b))
+# print("div:",z(a,b))
+
+c= lambda : "hello"
+print(c())
