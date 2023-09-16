86c4f5a9e8d7926dc8d57042bdf65ea2076f2729
gayathry2002 <139884338+gayathry2002@users.noreply.github.com>
2023-09-16 17:03:48 +0530

    message

---
+++ e.py
@@ -0,0 +1,56 @@
+# a=5 
+
+# try:
+#     print(a/b) 
+# except:
+#     print("error happend")
+
+# print("hello") 
+
+
+# try:
+#    n=9
+#    d=0
+#    result=n/d
+#    print(result)
+# except:
+#    print("error:d cannot be 0")
+# print("hyy")
+
+
+
+# a=10
+# try:
+#    print(a)
+# except:
+#    print("error happend:::")
+#    print("program continue.....")
+# else:
+#    print("valid")
+# finally:
+#    print("END!")
+
+
+# a=-5
+# if a>0:
+#     print("yes")
+# else:
+#     raise Exception("error!")
+
+
+# try:
+#    a=int(input("::"))
+# except:
+#    print("invalid")
+
+try:
+    a=int(input("::"))
+    b=int(input("::"))
+    s=a+b
+    print(s)
+except:
+    print("invalid")
+   
+   
+   
+
