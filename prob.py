7f59235d67b4e1b01b2b69c54957c3cc84f32425
gayathry <gayathripalliyath@gmail.com>
2023-09-18 16:42:20 +0530

    adding prb.py

---
+++ prob.py
@@ -0,0 +1,33 @@
+# a=input("::")
+# vowel=['a','e','i','o','u']
+# l=[]  
+# char=" "
+
+# for i in a:
+#     if i in vowel:
+#         l.append(i)
+# r=l[::-1] 
+
+# l1=list(a)  
+# l1[1]=r[0]
+# l1[4]=r[1]  
+
+
+# for i in l1:
+#      char+=i
+# print(char)
+  
+
+
+
+a=input("::")
+vowel=['a','e','i','o','u']
+l=[]  
+char=" "
+
+for i in a:
+    if i in vowel:
+        l.append(i)
+        r=l[::-1]
+        l1=list(a)
+print(l1) 
